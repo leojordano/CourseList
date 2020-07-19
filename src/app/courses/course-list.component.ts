55684b70c5968dcf69ea6a6556ecb549c3bc9f63
leojordano <leonardojordano1@gmail.com>
2020-07-19 20:09:07 -0300

    First Commit

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core'
+
+import {Course} from './course'
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html',
+})
+export class CourseListComponent implements OnInit {
+
+    courses:Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular Forms',
+                imgUrl: '/assets/images/forms.png',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'November, 2, 2019'
+            },
+
+            {
+                id: 2,
+                name: 'Angular HTTP',
+                imgUrl: '/assets/images/http.png',
+                price: 49.99,
+                code: 'LKL-8796',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'September, 27, 2019'
+            },
+        ]
+    }
+}
