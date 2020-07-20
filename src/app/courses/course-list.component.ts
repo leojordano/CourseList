7c17a9ac5a705a3c0271509396a9e3a0914683e7
leojordano <leonardojordano1@gmail.com>
2020-07-19 23:12:38 -0300

    Find and Edit

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,38 +1,34 @@
 import { Component, OnInit } from '@angular/core'
+import { CourseService } from './course.service'
 
 import {Course} from './course'
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html',
 })
 export class CourseListComponent implements OnInit {
 
-    courses:Course[] = [];
+    _courses:Course[] = []
+
+    filteredCourses:Course[] = []
+    _filterBy:string
+
+    constructor(private courseService: CourseService) {}
 
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular Forms',
-                imgUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 5.4,
-                releaseDate: 'November, 2, 2019'
-            },
+       this._courses = this.courseService.retriveAll()
+       this.filteredCourses = this._courses
+    }
 
-            {
-                id: 2,
-                name: 'Angular HTTP',
-                imgUrl: '/assets/images/http.png',
-                price: 49.99,
-                code: 'LKL-8796',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'September, 27, 2019'
-            },
-        ]
+    set filter(value:string) {
+        this._filterBy = value
+
+        this.filteredCourses = this._courses.filter((course:Course) => {
+           return course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1
+        })
+    }   
+
+    get filter() {
+        return this._filterBy
     }
 }
---
+++ src/app/navbar/navbar-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: "app-nav-bar",
+    templateUrl: "./navbar-bar.component.html"
+})
+export class NavBarComponent {
+
+}
